1d4aa616f38f351c5c4d7f03a4f071a327215415
K-Rithz <sothearith.kong@student.cadt.edu.kh>
2026-02-05 11:19:25 +0700

    W4-Practice

---
+++ exercise-3/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
---
+++ exercise2/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
